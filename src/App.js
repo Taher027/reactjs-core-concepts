c1d1acf7084c7a351e99c2bac77bdc5804e81ba8
Taher027 <abutaherh27@gmail.com>
2022-09-22 09:14:41 +0600

    practice

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-
+      <DisplaySomething name="taher" address="barishal"></DisplaySomething>
+      <DisplaySomething name="rohim" address="dhaka"></DisplaySomething>
+      <DisplaySomething name="karim" address="kishorgang"></DisplaySomething>
     </div>
   );
 }
 
 
-function DisplaySomething() {
-
+const DisplayStyle = {
+  backgroundColor: 'gray',
+  margin: '30px',
+  padding: '20px',
+  borderRadius: '15px'
+}
+function DisplaySomething(props) {
+  const [power, setPower] = useState(1);
+  const boostPower = () => {
+    const newPower = power * 2;
+    setPower(newPower);
+  }
+  return (
+    <div style={DisplayStyle}>
+      <p>Name: {props.name}</p>
+      <p>Addresss: {props.address}</p>
+      <h4>Power:{power} </h4>
+      <button onClick={boostPower}>Boost The Power</button>
+    </div>
+  )
 }
 
 export default App;
